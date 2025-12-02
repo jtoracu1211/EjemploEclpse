50062fca73a0777e935ec0e0b91a77725e9f297a
Juanma <jtoracu1211@g.educaand.es>
2025-12-02 20:00:48 +0100

    Añadimos "Adiós mundo cruel"

--- src/Ejemplo.java
+++ src/Ejemplo.java
@@ -3,5 +3,6 @@ public class Ejemplo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo");
+		System.out.println("Adiós mundo cruel");
 	}
 }
